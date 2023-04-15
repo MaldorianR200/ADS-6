4f447da4d4838ffd3693e923986072273979bcd5
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-04-15 19:59:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,26 @@
 // Copyright 2021 NNTU-CS
+#include "tpqueue.h"
+void push(SYM a) {
+  SYM* temp = new SYM(a.ch, a.prior, nullptr);
+  if (!head) {
+    head = temp;
+    tail = temp;
+  } else if (a.prior > head->prior) {
+    temp->next = head;
+    head = temp;
+  } else {
+    SYM* current = head;
+    while (current->next && a.prior <= current->next->prior)
+      current = current->next;
+    temp->next = current->next;
+    current->next = temp;
+  }
+}
+SYM pop() {
+  SYM* temp = head;
+  head = head->next;
+  return *temp;
+}
+bool isEmpty() const {
+  return head == nullptr;
+}
